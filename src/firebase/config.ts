4119246f541ff69a480c0a5a9f93cbac4dd4f759
Daniel Agudelo <danielote1010@gmail.com>
2025-10-31 00:11:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3540704341-17e37",
+  "appId": "1:750631025959:web:03c834e4d4ac72f0e0fd02",
+  "apiKey": "AIzaSyANBZc1wX_mwxgp7dUBVJOrzUrxlVt2bJA",
+  "authDomain": "studio-3540704341-17e37.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "750631025959"
+};
