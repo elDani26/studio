9bf0cc3e5e6f36933207f0eaaa3563a2a74d9b56
Daniel Agudelo <danielote1010@gmail.com>
2025-11-04 06:09:14 +0000

    Try fixing this error: `Console Error: A `locale` is expected to be retu

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,5 +4,6 @@ export const locales = ['es', 'en', 'fr', 'it', 'pt', 'zh'];
 export const localePrefix = 'always'; // Default
 
 export default getRequestConfig(async ({locale}) => ({
+  locale: locale,
   messages: (await import(`../messages/${locale}.json`)).default
 }));
