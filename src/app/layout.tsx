cb32f44b347a93adbe2fd0cae8e933423c7d15b2
Daniel Agudelo <danielote1010@gmail.com>
2025-11-07 22:20:39 +0000

    Try fixing this error: `Runtime Error: Error: Missing <html> and <body>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,43 @@
-// This is the root layout component for your Next.js app.
-// It applies to all routes.
+import type { Metadata } from 'next';
+import { FirebaseClientProvider } from '@/firebase';
+import { Toaster } from '@/components/ui/toaster';
+import './globals.css';
+import { PT_Sans } from 'next/font/google';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
+import {unstable_setRequestLocale} from 'next-intl/server';
+import {locales} from '../i18n';
 
-import { ReactNode } from 'react';
+const ptSans = PT_Sans({ subsets: ['latin'], weight: ['400', '700'] });
 
-type Props = {
-  children: ReactNode;
+export const metadata: Metadata = {
+  title: 'GestionaTuDinero',
+  description: 'Tu gestor de finanzas personales.',
 };
 
-// Since we have a `not-found.tsx` page on the root, a layout file
-// is required, even if it's just passing children through.
-export default function RootLayout({children}: Props) {
-  return children;
+export function generateStaticParams() {
+  return locales.map((locale) => ({locale}));
+}
+
+export default function RootLayout({
+  children,
+  params: {locale}
+}: Readonly<{
+  children: React.ReactNode;
+  params: {locale: string};
+}>) {
+  unstable_setRequestLocale(locale);
+  const messages = useMessages();
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={ptSans.className}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <FirebaseClientProvider>
+            {children}
+            <Toaster />
+          </FirebaseClientProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
